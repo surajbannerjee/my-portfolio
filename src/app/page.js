5a3ffdb8a8f039afc919a4ba66452aa65f4f9115
Suraj Banerjee <surajbanerjee255@gmail.com>
2025-09-01 13:47:10 +0530

    Initialize Next.js project with basic setup
    Add initial project files including Next.js configuration, ESLint, Tailwind CSS, and project assets. Includes .gitignore, .gitattributes, README, config files, public assets, and starter source files for a new portfolio project.

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome </h1>
+      <p>This is a </p>
+    </div>
+  );
+}
---
+++ src/constants/images.js
@@ -0,0 +1,4 @@
+const URL = (url) => {
+  return url;
+};
+export const LOGO = URL("/assets/images/logo.png");
